e9babb0f28fc6c6b4f71469b9780a47ee54eef03
Luis Alberto Patiño Scozzese <luis.scozzese@abacois.com>
2023-06-12 09:58:35 -0400

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PWAForTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
