bdda9d8694ba9b48282f8ed3b5b1bc296165edf4
Luis Alberto Patiño Scozzese <luis.scozzese@abacois.com>
2023-06-12 22:26:20 -0400

    Rectificacion en DB

--- Models/DbA9acbfDbbibliotecaContext.cs
+++ Models/DbA9acbfDbbibliotecaContext.cs
@@ -51,7 +51,7 @@ public partial class DbA9acbfDbbibliotecaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("SECRET");
+        => optionsBuilder.UseSqlServer("server=SQL8003.site4now.net;database=db_a9acbf_dbbiblioteca;uid=db_a9acbf_dbbiblioteca_admin;pwd=LHurol2022");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
